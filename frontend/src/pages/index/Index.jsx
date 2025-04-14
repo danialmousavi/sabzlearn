490d8ca499a71da8e28d084be303a95f8303bf52
danialmousavi <danialmoosavi69@gmail.com>
2025-04-14 21:34:02 +0330

    build article boxes and footer

--- frontend/src/pages/index/Index.jsx
+++ frontend/src/pages/index/Index.jsx
@@ -6,6 +6,7 @@ import AboutUs from '../../components/AboutUs/AboutUs'
 import PopularCourses from '../../components/PopularCourses/PopularCourses'
 import PresellCourses from '../../components/PresellCourses/PresellCourses'
 import LastArticles from '../../components/LastArticles/LastArticles'
+import Footer from '../../components/Footer/Footer'
 export default function Index() {
   return (
     <>
@@ -15,6 +16,7 @@ export default function Index() {
     <PopularCourses/>
     <PresellCourses/>
     <LastArticles/>
+    <Footer/>
     </>
 )
 }
