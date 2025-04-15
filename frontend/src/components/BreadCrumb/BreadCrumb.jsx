e01dfbc4b37c607e642738288c327f32357fed14
danialmousavi <danialmoosavi69@gmail.com>
2025-04-15 15:45:07 +0330

    add components to course Infos page(course progress bar, course detail boxes , course introduction)

---
+++ frontend/src/components/BreadCrumb/BreadCrumb.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './BreadCrumb.css'
+import {Link} from 'react-router'
+export default function BreadCrumb({links}) {
+  return (
+    <>
+            <section class="breadcrumb">
+      <div class="container">
+        <div class="breadcrumb__content">
+          <div class="breadcrumb__home-content-icon">
+            <i class="fas fa-home breadcrumb__home-icon"></i>
+          </div>
+          <ul class="breadcrumb__list">
+            {links.map(link=>(
+            <li class="breadcrumb__item">
+              <Link to={`/${link.to}`} class="breadcrumb__link">
+                {link.title}
+                
+                {link.id!=link.length?(
+                <i class="fas fa-angle-left breadcrumb__icon"></i>  
+                ):null}
+              </Link>
+            </li>
+            ))}
+
+          </ul>
+        </div>
+      </div>
+    </section>
+    </>
+)
+}
