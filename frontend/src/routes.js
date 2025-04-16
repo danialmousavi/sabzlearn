0092df4ebe0a89abdbc0dfbfcc7fab6c6169475d
danialmousavi <danialmoosavi69@gmail.com>
2025-04-16 12:55:07 +0330

    build register and login pages

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,6 +3,8 @@ import CategoryInfo from "./pages/Category/CategoryInfo";
 import CourseInfo from "./pages/courseinfo/CourseInfo";
 import Courses from "./pages/Courses/Courses";
 import Index from "./pages/index/Index";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
 
 const Routes=[
     {path:'/',element:<Index/>},
@@ -10,6 +12,8 @@ const Routes=[
     {path:'/course-info/:courseName',element:<CourseInfo/>},
     {path:'/article-info/:articleName',element:<ArticleInfo/>},
     {path:'/courses',element:<Courses/>},
+    {path:'/login',element:<Login/>},
+    {path:'/register',element:<Register/>},
 
 ]
 export default Routes
