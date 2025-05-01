893ee93b72a4391b22f1527a68e0ea3f3a6d144f
danialmousavi <danialmoosavi69@gmail.com>
2025-05-01 13:18:48 +0330

    courseDisplay type in category info page

--- frontend/src/pages/Category/CategoryInfo.jsx
+++ frontend/src/pages/Category/CategoryInfo.jsx
@@ -5,65 +5,69 @@ import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
 import CourseBox from "../../components/CourseBox/CourseBox";
 import Pagination from "../../components/Pagination/Pagination";
-import { useParams } from "react-router";
+import { Link, useParams } from "react-router";
 export default function CategoryInfo() {
-  const {categoryName}=useParams();
-  const [courses,setCourses]=useState([]);
-  const [shownCourses,setShownCourses]=useState([]);  
-  const [status,setStatus]=useState('default');
-  const [orderdCourses,setOrderedCourses]=useState([]);
-  const [statusTitle,setStatusTitle]=useState('مرتب سازی پیش فرض');
-  const [searchValue,setSearchValue]=useState('');
-  useEffect(()=>{
-    fetch(`http://localhost:3000/v1/courses/category/${categoryName}`).then(res=>res.json()).then(data=>setCourses(data)
-    )
-  },[categoryName]);
-  useEffect(()=>{
-    switch(status){
-      case 'free':{ 
-        setOrderedCourses(courses.filter(course=>course.price===0));  
+  const { categoryName } = useParams();
+  const [courses, setCourses] = useState([]);
+  const [shownCourses, setShownCourses] = useState([]);
+  const [status, setStatus] = useState("default");
+  const [orderdCourses, setOrderedCourses] = useState([]);
+  const [statusTitle, setStatusTitle] = useState("مرتب سازی پیش فرض");
+  const [searchValue, setSearchValue] = useState("");
+  const [coursTypeDisplay, setCourseTypeDisplay] = useState("row");
+  useEffect(() => {
+    fetch(`http://localhost:3000/v1/courses/category/${categoryName}`)
+      .then((res) => res.json())
+      .then((data) => setCourses(data));
+  }, [categoryName]);
+  useEffect(() => {
+    switch (status) {
+      case "free": {
+        setOrderedCourses(courses.filter((course) => course.price === 0));
         break;
       }
-      case 'money':{
-        setOrderedCourses(courses.filter(course=>course.price!==0));
-        break
+      case "money": {
+        setOrderedCourses(courses.filter((course) => course.price !== 0));
+        break;
       }
-      case 'latest':{
+      case "latest": {
         setOrderedCourses(courses);
         break;
       }
-      case 'avalin':{
-        const reversedCourses=courses.slice().reverse();
-        setOrderedCourses(reversedCourses)
+      case "avalin": {
+        const reversedCourses = courses.slice().reverse();
+        setOrderedCourses(reversedCourses);
         break;
       }
 
-      default:{
+      default: {
         setOrderedCourses(courses);
-        break
+        break;
       }
     }
-  },[status])
+  }, [status]);
   useEffect(() => {
     setShownCourses(courses); // Show the first 3 courses by default
     setOrderedCourses(courses); // Set the ordered courses to the full list initially
   }, [courses]);
-const handleStausTitle=(e)=>{
-setStatusTitle(e.target.innerText);
-}
-// useEffect(()=>{
-// if(searchValue.length>0){
-//   const filteredCourses=orderdCourses.filter(course=>course.name.toLowerCase().includes(searchValue.toLowerCase()));
-//   setShownCourses(filteredCourses);
-// }else{
-//   setShownCourses(orderdCourses);
-// }
-// }, [searchValue]);
-const handleSearch=(e)=>{
-  setSearchValue(e.target.value);
-  const filteredCourses=orderdCourses.filter(course=>course.name.toLowerCase().includes(e.target.value.toLowerCase()));
-  setShownCourses(filteredCourses);
-}
+  const handleStausTitle = (e) => {
+    setStatusTitle(e.target.innerText);
+  };
+  // useEffect(()=>{
+  // if(searchValue.length>0){
+  //   const filteredCourses=orderdCourses.filter(course=>course.name.toLowerCase().includes(searchValue.toLowerCase()));
+  //   setShownCourses(filteredCourses);
+  // }else{
+  //   setShownCourses(orderdCourses);
+  // }
+  // }, [searchValue]);
+  const handleSearch = (e) => {
+    setSearchValue(e.target.value);
+    const filteredCourses = orderdCourses.filter((course) =>
+      course.name.toLowerCase().includes(e.target.value.toLowerCase())
+    );
+    setShownCourses(filteredCourses);
+  };
   return (
     <>
       <Topbar />
@@ -72,47 +76,76 @@ const handleSearch=(e)=>{
         <div className="container">
           <div className="courses-top-bar">
             <div className="courses-top-bar__right">
-              <div className="courses-top-bar__row-btn courses-top-bar__icon--active">
+              <div
+                className={`courses-top-bar__row-btn ${
+                  coursTypeDisplay == "row"
+                    ? "courses-top-bar__icon--active"
+                    : ""
+                }`}
+                onClick={() => setCourseTypeDisplay("row")}
+              >
                 <i className="fas fa-border-all courses-top-bar__icon"></i>
               </div>
-              <div className="courses-top-bar__column-btn">
+              <div
+                className={`courses-top-bar__column-btn ${
+                  coursTypeDisplay == "column"
+                    ? "courses-top-bar__icon--active"
+                    : ""
+                }`}
+                onClick={() => setCourseTypeDisplay("column")}
+              >
                 <i className="fas fa-align-left courses-top-bar__icon"></i>
               </div>
 
               <div className="courses-top-bar__selection">
                 <span className="courses-top-bar__selection-title">
-                    {statusTitle}
+                  {statusTitle}
                   <i className="fas fa-angle-down courses-top-bar__selection-icon"></i>
                 </span>
                 <ul className="courses-top-bar__selection-list">
-
-                  <li className="courses-top-bar__selection-item courses-top-bar__selection-item--active" onClick={(e)=>{
-                    setStatus('default')
-                    handleStausTitle(e)
-                    }}>
-                  مرتب سازی پیش فرض 
+                  <li
+                    className="courses-top-bar__selection-item courses-top-bar__selection-item--active"
+                    onClick={(e) => {
+                      setStatus("default");
+                      handleStausTitle(e);
+                    }}
+                  >
+                    مرتب سازی پیش فرض
                   </li>
-                  <li className="courses-top-bar__selection-item" onClick={(e)=>{
-                    setStatus('free')
-                    handleStausTitle(e)
-                    }}>
+                  <li
+                    className="courses-top-bar__selection-item"
+                    onClick={(e) => {
+                      setStatus("free");
+                      handleStausTitle(e);
+                    }}
+                  >
                     مربت سازی دوره های رایگان
                   </li>
-                  <li className="courses-top-bar__selection-item" onClick={(e)=>{
-                    setStatus('money')
-                    handleStausTitle(e)
-                    }}>
+                  <li
+                    className="courses-top-bar__selection-item"
+                    onClick={(e) => {
+                      setStatus("money");
+                      handleStausTitle(e);
+                    }}
+                  >
                     مربت سازی بر دوره های پولی
                   </li>
-                  <li className="courses-top-bar__selection-item" onClick={(e)=>{
-                    setStatus('latest')
-                    handleStausTitle(e)
-                    }}>
+                  <li
+                    className="courses-top-bar__selection-item"
+                    onClick={(e) => {
+                      setStatus("latest");
+                      handleStausTitle(e);
+                    }}
+                  >
                     مربت سازی بر اساس آخرین
                   </li>
-                  <li className="courses-top-bar__selection-item" onClick={(e)=>{
-                    setStatus('avalin')
-                    handleStausTitle(e)}}>
+                  <li
+                    className="courses-top-bar__selection-item"
+                    onClick={(e) => {
+                      setStatus("avalin");
+                      handleStausTitle(e);
+                    }}
+                  >
                     مربت سازی بر اساس اولین
                   </li>
                 </ul>
@@ -134,17 +167,96 @@ const handleSearch=(e)=>{
           <div className="courses-content">
             <div className="container">
               <div className="row">
-                {shownCourses.length===0?(
+                {shownCourses.length === 0 ? (
                   <>
                     <div className="alert alert-warning">
                       هیچ دوره ای برای این دسته بندی وجود ندارد
                     </div>
                   </>
-                ):(
+                ) : (
                   <>
-                {shownCourses.map(course=>(
-                <CourseBox {...course}/>
-                ))}
+                    {coursTypeDisplay == "row" ? (
+                      <>
+                        {shownCourses.map((course) => (
+                          <CourseBox {...course} />
+                        ))}
+                      </>
+                    ) : (
+                      <>
+                        {shownCourses.map((course) => (
+                          <div class="col-12">
+                            <div class="course-box">
+                              <div class="course__box-header">
+                                <div class="course__box-right">
+                                  <Link
+                                    class="course__box-right-link"
+                                    to={`/course-info/${course.shortName}`}
+                                  >
+                                    <img
+                                      src={course.cover}
+                                      class="course__box-right-img"
+                                    />
+                                  </Link>
+                                </div>
+                                <div class="course__box-left">
+                                  <div class="course__box-left-top">
+                                    <Link
+                                      to={`/course-info/${course.shortName}`}
+                                      class="course__box-left-link"
+                                    >
+                                      {course.name}
+                                    </Link>
+                                  </div>
+                                  <div class="course__box-left-center">
+                                    <div class="course__box-left-teacher">
+                                      <i class="course__box-left-icon fa fa-chalkboard-teacher"></i>
+                                      <span class="course__box-left-name">
+                                        محمد امین سعیدی راد
+                                      </span>
+                                    </div>
+                                    <div class="course__box-left-stars">
+                                      <span class="course__box-left-star">
+                                        <img src="/images/svgs/star_fill.svg" />
+                                      </span>
+                                      <span class="course__box-left-star">
+                                        <img src="/images/svgs/star_fill.svg" />
+                                      </span>
+                                      <span class="course__box-left-star">
+                                        <img src="/images/svgs/star_fill.svg" />
+                                      </span>
+                                      <span class="course__box-left-star">
+                                        <img src="/images/svgs/star_fill.svg" />
+                                      </span>
+                                      <span class="course__box-left-star">
+                                        <img src="/images/svgs/star_fill.svg" />
+                                      </span>
+                                    </div>
+                                  </div>
+                                  <div class="course__box-left-bottom">
+                                    <div class="course__box-left-des">
+                                      <p>{course.description}</p>
+                                    </div>
+                                  </div>
+                                  <div class="course__box-footer">
+                                    <div class="course__box-footer-right">
+                                      <i class="course__box-footer-icon fa fa-users"></i>
+                                      <span class="course__box-footer-count">
+                                        202
+                                      </span>
+                                    </div>
+                                    <span class="course__box-footer-left">
+                                      {course.price === 0
+                                        ? "رایگان"
+                                        : course.price.toLocaleString()}
+                                    </span>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        ))}
+                      </>
+                    )}
                   </>
                 )}
               </div>
@@ -152,10 +264,10 @@ const handleSearch=(e)=>{
           </div>
         </div>
         <Pagination
-             items={orderdCourses}
-             itemCount={3}
-            pathName={`/category-info/${categoryName}`}
-            setShownCourses={setShownCourses}        
+          items={orderdCourses}
+          itemCount={3}
+          pathName={`/category-info/${categoryName}`}
+          setShownCourses={setShownCourses}
         />
       </section>
       <Footer />
