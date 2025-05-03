b9cfd45f77cad7a508ac30fe4b2d83ca74ca0cc0
danialmousavi <danialmoosavi69@gmail.com>
2025-05-03 13:59:33 +0330

    handle no notifcation

--- frontend/src/components/AdminPanel/Topbar/Topbar.jsx
+++ frontend/src/components/AdminPanel/Topbar/Topbar.jsx
@@ -1,63 +1,100 @@
-import React, { useEffect, useState } from 'react'
-import './Topbar.css'
+import React, { useEffect, useState } from "react";
+import "./Topbar.css";
 export default function Topbar() {
-  const [adminData,setAdminData]=useState({});
-  const [notification,setNotification]=useState([]);
-  const [showNotification,setShowNotification]=useState(false); 
+  const [adminData, setAdminData] = useState({});
+  const [notification, setNotification] = useState([]);
+  const [showNotification, setShowNotification] = useState(false);
   //fetch admin data
-  const fetchData=()=>{
-    const localStorageData=JSON.parse(localStorage.getItem('user'));
-    
-    fetch('http://localhost:3000/v1/auth/me',{
-      headers:{
-        "Authorization":`Bearer ${localStorageData}`
-      }
-    }).then(res=>res.json()).then(data=>{
-      setAdminData(data);
-      setNotification(data.notifications);
-    }
-    )
-  }
-  useEffect(()=>{
-    fetchData();
-  },[])
-  // change notification status
-  const seeNotification=(id)=>{
-    const localStorageData=JSON.parse(localStorage.getItem('user'));
-    fetch(`http://localhost:3000/v1/notifications/see/${id}`,{
-      method:"PUT",
-      headers:{
-        "Authorization":`Bearer ${localStorageData}`
-      }
-    }).then(res=>res.json().then(data=>{
-      fetchData()
+  const fetchData = () => {
+    const localStorageData = JSON.parse(localStorage.getItem("user"));
+
+    fetch("http://localhost:3000/v1/auth/me", {
+      headers: {
+        Authorization: `Bearer ${localStorageData}`,
+      },
     })
-    )
-  }
+      .then((res) => res.json())
+      .then((data) => {
+        setAdminData(data);
+        setNotification(data.notifications);
+      });
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  // change notification status
+  const seeNotification = (id) => {
+    const localStorageData = JSON.parse(localStorage.getItem("user"));
+    fetch(`http://localhost:3000/v1/notifications/see/${id}`, {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${localStorageData}`,
+      },
+    }).then((res) =>
+      res.json().then((data) => {
+        fetchData();
+      })
+    );
+  };
   return (
     <div className="container-fluid">
       <div className="container">
-        <div class={`home-header ${showNotification&&"active-modal-notfication"}`}>
+        <div
+          class={`home-header ${
+            showNotification && "active-modal-notfication"
+          }`}
+        >
           <div className="home-right">
             <div className="home-searchbar ">
-              <input type="text" className="search-bar" placeholder="جستجو..." />
+              <input
+                type="text"
+                className="search-bar"
+                placeholder="جستجو..."
+              />
             </div>
             <div className="home-notification ">
-              <button type="button" onMouseEnter={()=>setShowNotification(true)}>
+              <button
+                type="button"
+                onMouseEnter={() => setShowNotification(true)}
+              >
                 <i className="far fa-bell"></i>
               </button>
             </div>
-            <div className="home-notification-modal " onMouseEnter={()=>setShowNotification(true)} onMouseLeave={()=>setShowNotification(false)}>
+            <div
+              className="home-notification-modal "
+              onMouseEnter={() => setShowNotification(true)}
+              onMouseLeave={() => setShowNotification(false)}
+            >
               <ul className="home-notification-modal-list ">
-                {notification&&notification.map((item)=>(
-                <li className="home-notification-modal-item" key={item._id}>
-                <span className="home-notification-modal-text">{item.msg}</span>
-                <label className="switch">
-                  <button className='btn btn-success' onClick={()=>seeNotification(item._id)}>دیدم</button>
-                </label>
-              </li>
-                ))}
-
+                {notification.length === 0 ? (
+                  <>
+                    <li className="home-notification-modal-item alert alert-warning">
+                        هیچ پیفامی وجود نداد!
+                    </li>
+                  </>
+                ) : (
+                  <>
+                    {notification &&
+                      notification.map((item) => (
+                        <li
+                          className="home-notification-modal-item"
+                          key={item._id}
+                        >
+                          <span className="home-notification-modal-text">
+                            {item.msg}
+                          </span>
+                          <label className="switch">
+                            <button
+                              className="btn btn-success"
+                              onClick={() => seeNotification(item._id)}
+                            >
+                              دیدم
+                            </button>
+                          </label>
+                        </li>
+                      ))}
+                  </>
+                )}
               </ul>
             </div>
           </div>
@@ -79,5 +116,5 @@ export default function Topbar() {
         </div>
       </div>
     </div>
-  )
+  );
 }
