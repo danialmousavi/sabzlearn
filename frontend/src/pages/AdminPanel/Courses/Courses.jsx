a4673957e2551b6a158f4cad9a01cb7bad20dce8
danialmousavi <danialmoosavi69@gmail.com>
2025-05-05 22:48:20 +0330

    show courses in p-admin

--- frontend/src/pages/AdminPanel/Courses/Courses.jsx
+++ frontend/src/pages/AdminPanel/Courses/Courses.jsx
@@ -1,7 +1,61 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import DataTable from '../../../components/AdminPanel/DataTable/DataTable'
 
 export default function Courses() {
+  const [Courses,setCourses]=useState([]);
+  useEffect(()=>{
+    const localStorageData=JSON.parse(localStorage.getItem('user'));
+    fetch('http://localhost:3000/v1/courses',{
+      headers:{
+        "Authorization":`Bearer ${localStorageData}`
+      }
+    }).then((res)=>res.json()).then(data=>setCourses(data))
+  },[])
+  console.log(Courses);
+  
   return (
-    <div>Courses</div>
+    <>
+        <DataTable title="دوره ها">
+        <table class="table">
+          <thead>
+            <tr>
+              <th>شناسه</th>
+              <th>عنوان</th>
+              <th>مبلغ</th>
+              <th>وضعیت برگذاری</th>
+              <th>لینک</th>
+              <th>مدرس</th>
+              <th>نحوه پشتیبانی</th>
+
+            </tr>
+          </thead>
+          <tbody>
+            {Courses&&Courses.map((course,index)=>(
+            <tr>
+              <td>{index+1}</td>
+              <td>{course.name}</td>
+              <td>{course.price==0?'رایگان':course.price}</td>
+              <td>{course.isComplete?"تکمیل شده":"درحال برگذاری"}</td>
+              <td>{course.shortName}</td>
+              <td>{course.creator.name}</td>
+              <td>{course.support}</td>
+
+              <td>
+                <button type="button" class="btn btn-primary edit-btn">
+                  ویرایش
+                </button>
+              </td>
+              <td>
+                <button type="button" class="btn btn-danger delete-btn" >
+                  حذف
+                </button>
+              </td>
+
+            </tr>
+            ))}
+          </tbody>
+        </table>
+        </DataTable>
+    </>
   )
 }
