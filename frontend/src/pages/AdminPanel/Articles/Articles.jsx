4fa14e4ba1e49a0b575f656301b1c3acc1229498
danialmousavi <danialmoosavi69@gmail.com>
2025-05-09 22:33:59 +0330

    show articles list

--- frontend/src/pages/AdminPanel/Articles/Articles.jsx
+++ frontend/src/pages/AdminPanel/Articles/Articles.jsx
@@ -1,7 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import DataTable from '../../../components/AdminPanel/DataTable/DataTable';
 
 export default function Articles() {
+  const [allArticles,setAllArticles]=useState([]);
+  useEffect(()=>{
+    fetch('http://localhost:3000/v1/articles').then(res=>res.json()).then(data=>{
+      setAllArticles(data);
+      console.log(data);
+    })
+  },[])
   return (
-    <div>Articles</div>
+    <>
+    <DataTable title='مقاله ها'>
+           <table class="table">
+          <thead>
+            <tr>
+              <th>شناسه</th>
+              <th>اسم مقاله</th>
+              <th>لینک مقاله</th>
+              <th>نویسنده</th>
+              <th>ویرایش</th>
+              <th>حذف</th>
+            </tr>
+          </thead>
+          <tbody>
+            {allArticles&&allArticles.map((article,index)=>(
+            <tr key={article._id}>
+              <td>{index+1}</td>
+              <td>{article.title}</td>
+              <td>{article.shortName}</td>
+              <td>{article.creator.name}</td>
+              <td>
+                <button type="button" class="btn btn-primary edit-btn">
+                  ویرایش
+                </button>
+              </td>
+              <td>
+                <button type="button" class="btn btn-danger delete-btn">
+                  حذف
+                </button>
+              </td>
+
+            </tr>
+            ))}
+          </tbody>
+        </table>
+    </DataTable>
+    </>
   )
 }
