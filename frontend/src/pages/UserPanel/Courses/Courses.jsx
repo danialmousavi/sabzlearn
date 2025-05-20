3da60cbc05b282547e5b62ae51201cbcf0e0f093
danialmousavi <danialmoosavi69@gmail.com>
2025-05-20 15:43:35 +0330

    filter courses

--- frontend/src/pages/UserPanel/Courses/Courses.jsx
+++ frontend/src/pages/UserPanel/Courses/Courses.jsx
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from 'react'
 import './Coruses.css'
 
 export default function Courses() {
-    const [courses,setCourses]=useState([])
+    const [courses,setCourses]=useState([]);
+    const [showCoursesState,setShowCoursesState]=useState("all");
+    const [shownCourses,setShownCourses]=useState([]);
     useEffect(()=>{
     const localStorageData=JSON.parse(localStorage.getItem('user'));
         fetch(`http://localhost:3000/v1/users/courses/`,{
@@ -10,31 +12,66 @@ export default function Courses() {
                 Authorization: `Bearer ${localStorageData}`,
                      }
         }).then(res=>res.json()).then(data=>{
-            // console.log(data);
-            setCourses(data)
+            console.log(data);
+            setCourses(data);
+            setShownCourses(data);
         }
         )
     },[])
-    console.log(courses);
-    
+    //filtering the user courses
+    const filterCourses=(state)=>{
+        switch(state){
+            case 'all':{
+                setShownCourses(courses);
+                break;
+            }
+            case 'free':{
+                const filterdCourse=courses.filter(course=>course.course.price===0)
+                setShownCourses(filterdCourse);
+                break;
+            }
+            case 'money':{
+                const filterdCourse=courses.filter(course=>course.course.price!==0)
+
+                setShownCourses(filterdCourse);
+                break;
+            }
+            
+            default:{
+                setShownCourses(courses);
+            }
+        }
+    }
   return (
     <div className="col-9">
       <div className="courses">
         <div className="courses-header__panel">
           <span className="courses-header__title">دوره های ثبت نام شده</span>
           <ul className="courses-header__list">
-            <li className="courses-header__item">
-              <a className="courses-header__link__panel courses-header__link-active" href="#">
+            <li className="courses-header__item" >
+              <a className={`courses-header__link__panel ${showCoursesState==="all"?"courses-header__link-active":null}`} href="#" onClick={(e)=>{
+                e.preventDefault()
+                setShowCoursesState('all')
+                filterCourses('all')
+            }}>
                 همه دوره ها
               </a>
             </li>
             <li className="courses-header__item">
-              <a className="courses-header__link__panel" href="#">
+              <a className={`courses-header__link__panel ${showCoursesState==="free"?"courses-header__link-active":null}`}href="#" onClick={(e)=>{
+                e.preventDefault()
+                setShowCoursesState('free');
+                filterCourses('free')
+            }}>
                 دوره های رایگان
               </a>
             </li>
             <li className="courses-header__item">
-              <a className="courses-header__link__panel" href="#">
+              <a className={`courses-header__link__panel ${showCoursesState==="money"?"courses-header__link-active":null}`} href="#" onClick={(e)=>{
+                e.preventDefault()
+                setShowCoursesState('money');
+                filterCourses('money')
+            }}>
                 دوره های پولی
               </a>
             </li>
@@ -43,7 +80,7 @@ export default function Courses() {
         <div className="main">
           <div className="row">
             <div className="col-12">
-                {courses.map(course=>(
+                {shownCourses.map(course=>(
             <div className="main__box">
                 <div className="main__box-right">
                   <a className="main__box-img-link" href="#">
